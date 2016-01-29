672d65674baa47d183f974811c999a8a56b87526
Thomas Dybdal Pedersen <thomasp85@gmail.com>
2016-01-29 15:28:57 +0100

    Add layout_igraph_treemap

--- R/layout_igraph.R
+++ R/layout_igraph.R
@@ -271,6 +271,27 @@ layout_igraph_linear <- function(graph, circular, sort.by = NULL, use.numeric = 
     nodes$circular <- circular
     nodes
 }
+#' @importFrom igraph gorder as_edgelist delete_vertex_attr
+layout_igraph_treemap <- function(graph, algorithm = 'split', weight = NULL, circular = FALSE, sort.by = NULL, mode = 'out', height = 1, width = 1) {
+    graph <- graph_to_tree(graph, mode)
+    if (is.named(graph)) graph <- delete_vertex_attr(graph, 'name')
+    hierarchy <- tree_to_hierarchy(graph, mode, sort.by, weight)
+    layout <- switch(
+        algorithm,
+        split = splitTreemap(hierarchy$parent, hierarchy$order, hierarchy$weight, width, height),
+        stop('Unknown algorithm')
+    )
+    layout <- data.frame(x = layout[, 1] + layout[, 3]/2,
+                         y = layout[, 2] + layout[, 4]/2,
+                         width = layout[, 3],
+                         height = layout[, 4],
+                         circular = FALSE,
+                         leaf = degree(graph, mode = mode) == 0)
+    extraData <- as.data.frame(vertex_attr(graph))
+    if (nrow(extraData) == 0) extraData <- data.frame(row.names = seq_len(nrow(layout)))
+    layout <- cbind(layout, extraData)
+    layout
+}
 is.igraphlayout <- function(type) {
     if (type %in% igraphlayouts) {
         TRUE
@@ -292,6 +313,63 @@ as.igraphlayout <- function(type) {
         layout <- newType[typeInd]
     }
     paste0('layout_', layout)
+}
+#' @importFrom igraph degree unfold_tree components induced_subgraph vertex_attr_names
+graph_to_tree <- function(graph, mode) {
+    if (!is.directed(graph)) {
+        stop('Graph must be directed')
+    }
+    parentDir <- if (mode == 'out') 'in' else 'out'
+    comp <- components(graph, 'weak')
+    if (comp$no > 1) {
+        message('Multiple components in graph. Choosing the first')
+        graph <- induced_subgraph(graph, which(comp$membership == 1))
+    }
+    nParents <- degree(graph, mode = parentDir)
+    if (!any(nParents == 0)) {
+        stop('No root in graph. Provide graph with one parentless node')
+    }
+    if (any(nParents > 1)) {
+        message('Multiple parents. Unfolding graph')
+        root <- which(degree(graph, mode = parentDir) == 0)
+        if (length(root) > 1) {
+            message('Multiple roots in graph. Choosing the first')
+            root <- root[1]
+        }
+        tree <- unfold_tree(graph, mode = mode, roots = root)
+        vAttr <- lapply(vertex_attr(graph), `[`, i = tree$vertex_index)
+        vertex_attr(tree$tree) <- vAttr
+        graph <- tree$tree
+    }
+    graph
+}
+tree_to_hierarchy <- function(graph, mode, sort.by, weight) {
+    parentCol <- if (mode == 'out') 1 else 2
+    nodeCol <- if (mode == 'out') 2 else 1
+    edges <- as_edgelist(graph)
+    hierarchy <- data.frame(parent = rep(-1, gorder(graph)))
+    hierarchy$parent[edges[, nodeCol]] <- edges[, parentCol] - 1
+    if (is.null(sort.by)) {
+        hierarchy$order <- seq_len(nrow(hierarchy))
+    } else {
+        hierarchy$order <- order(vertex_attr(graph, sort.by))
+    }
+    leaf <- degree(graph, mode = mode) == 0
+    if (is.null(weight)) {
+        hierarchy$weight <- 0
+        hierarchy$weight[leaf] <- 1
+    } else {
+        weight <- vertex_attr(graph, weight)
+        if (!is.numeric(weight)) {
+            stop('Weight must be numeric')
+        }
+        hierarchy$weight <- weight
+        if (any(hierarchy$weight[!leaf] != 0)) {
+            message('Non-leaf weights ignored')
+        }
+        hierarchy$weight[!leaf] <- 0
+    }
+    hierarchy
 }
 igraphlayouts <- c(
     'as_bipartite',
