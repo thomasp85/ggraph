f526e0801b278cc3e3ba830db4c5f328cb730900
Thomas Dybdal Pedersen <thomasp85@gmail.com>
2016-02-02 12:53:46 +0100

    Adding some more datasets

---
+++ R/data_highschool.R
@@ -0,0 +1,20 @@
+#' Friendship among high school boys
+#'
+#' This dataset shows the friendship among high school boys as assessed by the
+#' question: "What fellows here in school do you go around with most often?".
+#' The question was posed twice, with one year in between (1957 and 1958) and
+#' shows the evolution in friendship between the two timepoints.
+#'
+#' @format
+#' The graph is stored as an unnamed edgelist with a year attribute.
+#' \describe{
+#'  \item{from}{The boy answering the question}
+#'  \item{to}{The boy being the answer to the question}
+#'  \item{year}{The year the friendship was reported}
+#' }
+#'
+#' @source
+#' Coleman, J. S. \emph{Introduction to Mathermatical Sociology}. New York: Free
+#' Press, pp.450-451.
+#'
+'highschool'
---
+++ R/data_whigs.R
@@ -0,0 +1,20 @@
+#' Membership network of American Whigs
+#'
+#' This dataset shows the membership of 136 colonial Americans in 5 whig
+#' organization and is a bipartite graph. The data appeared in the appendix to
+#' David Hackett Fischer's \emph{Paul Revere's Ride} (Oxford University Press,
+#' 1995) and compiled by Kieran Healy for the blog post
+#' \href{http://kieranhealy.org/blog/archives/2013/06/09/using-metadata-to-find-
+#' paul-revere/}{Using Metadata to Find Paul Revere}.
+#'
+#' @format
+#' The data is stored as an incidence matrix with persons as rows and
+#' organizations as columns. A 0 means no membership while a one means
+#' membership.
+#'
+#' @source \url{https://github.com/kjhealy/revere/blob/master/data/PaulRevereAppD.csv}
+#' adapted from:
+#'
+#' Fischer, David H. (1995) \emph{Paul Revere's Ride}. Oxford University Press
+#'
+'whigs'
