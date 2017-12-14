e928c3f6cb7da90d80d0a453f6384bdc0a9bb0f8
Thomas Lin Pedersen <thomasp85@gmail.com>
2017-12-14 10:00:25 +0100

    Use new roxygen package doc

--- R/ggraph-package.R
+++ R/ggraph-package.R
@@ -1,26 +1,5 @@
-#' ggraph: Grammar of Graph Graphics
-#'
-#' This package is an extension of the grammar of graphics defined in
-#' \code{\link{ggplot2}} for relational data such as networks and trees.
-#' \code{ggraph} adds a long list of new geoms and scales as well as a new plot
-#' constructor that helps you build up your network visualization layer by layer
-#' in the same manner as you know from \code{ggplot2}.
-#'
-#' @author Thomas Lin Pedersen \email{thomasp85@gmail.com}
-#'
-#' @seealso
-#' Useful links:
-#' \describe{
-#'   \item{Development version}{\url{https://github.com/thomasp85/ggraph}}
-#'   \item{Feature requests and bug reports}{\url{https://github.com/thomasp85/ggraph/issues}}
-#' }
-#'
+#' @aliases NULL
 #' @useDynLib ggraph
-#'
 #' @import ggplot2
 #'
-#' @docType package
-#' @name ggraph-package
-#' @rdname ggraph-package
-#'
-NULL
+'_PACKAGE'
