ca1f700ae00bd88c9897a508cacdf899a67d5cce
Thomas Lin Pedersen <thomasp85@gmail.com>
2018-01-12 11:06:01 +0100

    Fix #91
    Easier specification of manual layouts

--- R/layout_auto.R
+++ R/layout_auto.R
@@ -1,7 +1,8 @@
 #' Automatically pick a layout based on graph type
 #'
 #' This function infers the layout from the graph structure and is the default
-#' when calling [ggraph()]. If the graph is either a tree or a forest
+#' when calling [ggraph()]. If an `x` and `y` argument is passed along, the
+#' manual layout is chosen. Otherwise if the graph is either a tree or a forest
 #' the layout will be `dendrogram` if the nodes contains a height variable or
 #' `tree` if not. If the tree is a DAG the `sygiyama` layout will be used.
 #' Otherwise the `nicely` layout will be used.
@@ -19,10 +20,12 @@
 #'
 #' @family layout_tbl_graph_*
 #'
-#' @importFrom rlang .data
+#' @importFrom rlang .data quos
 #'
 layout_tbl_graph_auto <- function(graph, circular, ...) {
-    if (with_graph(graph, graph_is_tree() || graph_is_forest())) {
+    if (all(c('x', 'y') %in% names(quos(...)))) {
+        layout_tbl_graph_manual(graph, circular = circular, ...)
+    } else if (with_graph(graph, graph_is_tree() || graph_is_forest())) {
         if (is.null(with_graph(graph, .N()[['height']]))) {
             message('Using `tree` as default layout')
             layout_tbl_graph_igraph(graph, algorithm = 'tree', circular = circular, ...)
