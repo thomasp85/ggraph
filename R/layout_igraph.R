54b2f30d60796ba1b835daa3beb95c9d1e4ba1e2
Thomas Lin Pedersen <thomasp85@gmail.com>
2020-11-12 14:20:00 +0100

    Fix #160, close #161 warn when additional dimensions are requested in igraph layouts

--- R/layout_igraph.R
+++ R/layout_igraph.R
@@ -114,6 +114,9 @@ layout_tbl_graph_igraph <- function(graph, algorithm, circular, offset = pi / 2,
   if (algorithm == 'layout_as_tree') {
     layout[, 1] <- layout[, 1] + components(graph)$membership - 1
   }
+  if ('dim' %in% names(dots) && isTRUE(dots$dim > 2)) {
+    warning('Only the first two dimensions will be used despite requesting more', call. = FALSE)
+  }
   extra_data <- as_tibble(graph, active = 'nodes')
   layout <- cbind(x = layout[, 1], y = layout[, 2], extra_data[, !names(extra_data) %in% c('x', 'y'), drop = FALSE])
   graph <- add_direction(graph, layout)
