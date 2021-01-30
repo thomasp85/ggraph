aee6410bc35e5e196d7dc40173fa7f27d7f6c873
loreabad6 <lore.abad6@gmail.com>
2021-01-30 19:22:46 +0100

    Fix overwrite of columns with same name

--- R/layout_sf.R
+++ R/layout_sf.R
@@ -6,6 +6,8 @@
 #' @param graph An sfnetwork object
 #' @param circular ignored
 #'
+#' @importFrom tidygraph as_tbl_graph
+#'
 #' @return A data.frame with the columns `x`, `y`, `circular`.
 #'
 #' @family layout_tbl_graph_*
@@ -14,16 +16,21 @@ layout_tbl_graph_sf <- function(graph, circular = FALSE) {
   if (!requireNamespace("sf", quietly = TRUE)) {
     stop("Package sf required, please install it first.", call. = FALSE)
   }
+  # Check if network is an sfnetwork
+  if (!inherits(graph, "sfnetwork")) {
+    stop("Layout 'sf' needs an 'sfnetwork' graph.", call. = FALSE)
+  }
   # Extract X and Y coordinates from the nodes
   graph <- activate(graph, "nodes")
   x <- sf::st_coordinates(graph)[,"X"]
   y <- sf::st_coordinates(graph)[,"Y"]
   # Create layout data frame
   nodes <- new_data_frame(list(x = x, y = y))
-  extra_data <- sf::st_drop_geometry(as_tibble(graph, active = "nodes"))
+  nodes$circular <- FALSE
+  # Convert to tbl_graph to 'unstick' geometry column
+  extra_data <- as_tibble(as_tbl_graph(graph), active = "nodes")
   warn_dropped_vars(nodes, extra_data)
   nodes <- cbind(nodes, extra_data[, !names(extra_data) %in% names(nodes), drop = FALSE])
-  nodes$circular <- FALSE
   attr(nodes, 'graph') <- graph
   nodes
 }
