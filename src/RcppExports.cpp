1952710711cd27929b7dd1cd14d39f493a2fd88e
Thomas Lin Pedersen <thomasp85@gmail.com>
2022-08-08 09:54:05 +0200

    update rcpp

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // pack
 NumericMatrix pack(NumericVector areas);
 RcppExport SEXP _ggraph_pack(SEXP areasSEXP) {
